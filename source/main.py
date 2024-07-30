dde2dca51f31203701e96781f7b36acb290e18a5
Arkadiusz Polec <163996@stud.prz.edu.pl>
2024-07-30 06:05:28 +0200

    Add main.py for Discord bot initialization and logic

---
+++ source/main.py
@@ -0,0 +1,51 @@
+import asyncio
+from typing import List
+
+import discord
+from discord import Message
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+import os
+from difflib import SequenceMatcher
+from Levenshtein import distance
+from collect_data import collect_data
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+# Intents and token
+intents = discord.Intents.default()
+intents.message_content = True
+TOKEN = os.getenv('TOKEN')
+
+# Change the no_category default string
+help_command = commands.DefaultHelpCommand(
+    no_category='Commands',
+)
+
+# Set bot activity status
+activity = discord.Activity(type=discord.ActivityType.listening, name="m.help")
+
+# Bot
+bot = discord.ext.commands.Bot(command_prefix=["m."], activity=activity, case_insensitive=True, intents=intents,
+                               help_command=help_command)
+@bot.event
+async def on_ready() -> None:
+    print(f'Logged as: {bot.user}')
+    await update_data()
+    print("g_data size: ", len(bot.g_data))
+
+
+@tasks.loop(minutes=120)
+async def update_data() -> None:
+    print("Preparation for data collection...")
+    websites, data = await collect_data()
+
+    print("Loading data...")
+    bot.g_state = 0  # bot controls blocked
+    await asyncio.sleep(3)  # wait in case of handle state running
+    bot.g_websites, bot.g_data = websites, data
+    bot.g_state = 1
+    print(f"Using websites: {bot.g_websites}")
+
+bot.run(TOKEN)
