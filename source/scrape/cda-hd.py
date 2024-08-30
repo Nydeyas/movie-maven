0e2ad68c46417f473553df2350373f2c1c4d8a6f
Arkadiusz Polec <163996@stud.prz.edu.pl>
2024-08-30 18:12:37 +0200

    Update cda-hd.py

--- source/scrape/cda-hd.py
+++ source/scrape/cda-hd.py
@@ -22,6 +22,9 @@ def find_last_number(text: str) -> str:
 def scrape_movies(site_name: str, site_link: str, max_pages: int | None) -> List[MoviePayload]:
     browser = Driver(uc=True, headless=True)
     a = datetime.now()
+    movies = []
+
+    # Get number of pages with movies
     try:
         browser.get(f'{site_link}/page/1/')
         browser.sleep(2)
@@ -33,60 +36,56 @@ def scrape_movies(site_name: str, site_link: str, max_pages: int | None) -> List
         return []
 
     scheme_list = re.findall(r'\d+', href_last_page)
+    if not scheme_list:
+        browser.close()
+        logging.warning(f"Cannot process element: 'page_count', site_name = {site_name}")
+        return []
 
-    if scheme_list:
-        try:
-            movies = []
+    try:
+        pages_count = min(max_pages, int(scheme_list[0])) if max_pages else int(scheme_list[0])
 
-            # Check if maximum page limit is set
-            if max_pages:
-                pages_count = min(max_pages, int(scheme_list[0]))
-            else:
-                pages_count = int(scheme_list[0])
+        for page_number in range(1, pages_count + 1):
+            # Get page with movies
+            logging.info(f"Page: {page_number}/{pages_count}...")
 
-            for page_number in range(1, pages_count + 1):
-                # Page with movies
-                logging.info(f"Page: {page_number}/{pages_count}...")
+            browser.default_get(f'{site_link}/page/{page_number}/')
 
-                browser.default_get(f'{site_link}/page/{page_number}/')
+            sector = browser.find_element(By.CLASS_NAME, "item_1")
+            elements = sector.find_elements(By.CLASS_NAME, "item")
+            hrefs = [e.find_element(By.TAG_NAME, "a").get_attribute('href') for e in elements]
 
-                sector = browser.find_element(By.CLASS_NAME, "item_1")
-                elements = sector.find_elements(By.CLASS_NAME, "item")
-                hrefs = [e.find_element(By.TAG_NAME, "a").get_attribute('href') for e in elements]
-
-                for h in hrefs:
-                    # Movie url
-                    browser.default_get(h)
-
+            for h in hrefs:
+                # Single Movie url
+                browser.default_get(h)
+                try:
                     # XPATH - faster than CSS Selector
-                    obj = browser.find_element(By.XPATH, value="//*[@id='uwee']")
-                    title: str = obj.find_element(By.XPATH, value="./div[2]/h1").text
+                    title: str = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/h1").text
 
-                    d_elements = browser.find_elements(By.XPATH, value="//*[@id='cap1']/p")
-                    description: str = d_elements[0].text if d_elements else ""
+                    description_elements = browser.find_elements(By.XPATH, "//*[@id='cap1']/p")
+                    description: str = description_elements[0].text if description_elements else ""
 
                     show_type: str = "Film"
 
-                    tags_elements = obj.find_elements(By.XPATH, value=".//a[@rel='category tag']")
+                    tags_elements = browser.find_elements(By.XPATH, "//*[@id='uwee']//a[@rel='category tag']")
                     tags: str = ', '.join(t.text for t in tags_elements)
 
-                    year_text = obj.find_element(By.XPATH, value="./div[2]/span").text
+                    year_text = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/span").text
                     year_str = find_last_number(year_text)
                     year = int(year_str) if year_str.isdigit() else None
 
-                    length_text = obj.find_element(By.XPATH, "//*[contains(@class, 'icon-time')]/..").text
+                    length_text = browser.find_element(By.XPATH, "//*[contains(@class, 'icon-time')]/..").text
                     length_str = find_first_number(length_text)
                     length = int(length_str) if length_str.isdigit() else None
 
-                    rating_str = obj.find_element(By.XPATH, value="./div[2]/div[2]/a/div/span").text
+                    rating_str = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/div[2]/a/div/span").text
                     rating = float(rating_str) if rating_str.replace('.', '', 1).isdigit() else "N/A"
 
-                    votes_text = obj.find_element(By.XPATH, value="./div[2]/div[2]/div/span/b[2]").text
+                    votes_text = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/div[2]/div/span/b[2]").text
                     votes_str = find_first_number(votes_text)
                     votes = int(votes_str) if votes_str.isdigit() else None
 
-                    countries: str = obj.find_element(By.XPATH, value="./div[2]/p[4]").text
-                    image_link: str = obj.find_element(By.XPATH, value="./div[1]/div/img").get_attribute("src")
+                    countries = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/p[4]").text
+                    image_link = browser.find_element(By.XPATH, "//*[@id='uwee']/div[1]/div/img").get_attribute("src")
                     link: str = h
 
                     movies.append(
@@ -104,19 +103,18 @@ def scrape_movies(site_name: str, site_link: str, max_pages: int | None) -> List
                             image_link=image_link
                         )
                     )
-            logging.info(f"Found {len(movies)} movies.")
+                except Exception:
+                    logging.warning(f"Error extracting movie details for URL {h}")
+                    continue
 
-        except Exception as e:
-            logging.warning(f"Error 3 in cda-hd.get_movies(): {e}")
-            return []
+        logging.info(f"Found {len(movies)} movies.")
 
-    else:
+    except Exception as e:
+        logging.warning(f"Error scraping movies: {e}")
+
+    finally:
         browser.close()
-        logging.warning(
-            f"Error 2: Cannot process element: 'page_count' in cda-hd.get_movies(), site_name = {site_name}")
-        return []
 
     b = datetime.now()
     logging.info(f"cda-hd.get_movies() completed: {b - a}")
-    browser.close()
     return movies
