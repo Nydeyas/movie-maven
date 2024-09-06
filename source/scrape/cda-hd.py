d81f3e3a08460cf686c2df5c5c92678d69202f7d
Arkadiusz Polec <163996@stud.prz.edu.pl>
2024-09-06 13:36:12 +0200

    Improve script for CDA-HD

--- source/scrape/cda-hd.py
+++ source/scrape/cda-hd.py
@@ -1,21 +1,53 @@
+from selenium.common import NoSuchElementException
+
 from source.to_thread import to_thread
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
-from typing import List
+from typing import List, Any
 import re
 from source.classes.types_base import Movie as MoviePayload
 import logging
 from seleniumbase import Driver
 
 
-def find_first_number(text: str) -> str:
+def find_first_number(text: str, find_decimal: bool = False, remove_separator: bool = True) -> str:
     """Finds the first number in the text."""
-    return next((match.group() for match in re.finditer(r'\d+', text)), "")
+    if remove_separator:
+        text = text.replace(',', '')
+    pattern = r'\d+(\.\d+)?' if find_decimal else r'\d+'
+    match = re.search(pattern, text)
+    return match.group() if match else ""
 
 
-def find_last_number(text: str) -> str:
+def find_last_number(text: str, find_decimal: bool = False, remove_separator: bool = True) -> str:
     """Finds the last number in the text."""
-    return next((match.group() for match in reversed(list(re.finditer(r'\d+', text)))), "")
+    if remove_separator:
+        text = text.replace(',', '')
+    pattern = r'\d+(\.\d+)?' if find_decimal else r'\d+'
+    matches = list(re.finditer(pattern, text))
+    return matches[-1].group() if matches else ""
+
+
+def extract_minutes(text: str, default: Any = None) -> int | None:
+    """Extracts total minutes from a time string which could contain hours (h) and minutes (min)."""
+    text = text.lower()
+    total_minutes = 0
+
+    # Extract hours if present
+    hours_match = re.search(r'(\d+)\s*h', text)
+    if hours_match:
+        hours = hours_match.group(1)
+        total_minutes += int(hours) * 60
+
+    # Extract minutes if present
+    minutes_match = re.search(r'(\d+)\s*min', text)
+    if minutes_match:
+        minutes = minutes_match.group(1)
+        total_minutes += int(minutes)
+
+    return total_minutes if total_minutes else default
 
 
 @to_thread
@@ -27,8 +59,9 @@ def scrape_movies(site_name: str, site_link: str, max_pages: int | None) -> List
     # Get number of pages with movies
     try:
         browser.get(f'{site_link}/page/1/')
-        browser.sleep(2)
-        element_last_page = browser.find_element(By.LINK_TEXT, value="Ostatnia")
+        element_last_page = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, "Ostatnia"))
+        )
         href_last_page = element_last_page.get_attribute("href")
     except Exception as e:
         browser.close()
@@ -61,8 +94,11 @@ def scrape_movies(site_name: str, site_link: str, max_pages: int | None) -> List
                     # XPATH - faster than CSS Selector
                     title: str = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/h1").text
 
-                    description_elements = browser.find_elements(By.XPATH, "//*[@id='cap1']/p")
-                    description: str = description_elements[0].text if description_elements else ""
+                    try:
+                        description = browser.find_element(By.XPATH, "//*[@id='cap1']/p").text
+                    except NoSuchElementException:
+                        description_elements = browser.find_elements(By.XPATH, "//*[@id='cap1']/div/p")
+                        description: str = description_elements[0].text if description_elements else ""
 
                     show_type: str = "Film"
 
@@ -72,19 +108,23 @@ def scrape_movies(site_name: str, site_link: str, max_pages: int | None) -> List
                     year_text = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/span").text
                     year_str = find_last_number(year_text)
                     year = int(year_str) if year_str.isdigit() and 1900 < int(year_str) < 2100 else None
+                    if not year:
+                        year_str = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/p[1]/span[1]/a").text
+                        year = int(year_str) if year_str.isdigit() and 1900 < int(year_str) < 2100 else None
 
                     length_text = browser.find_element(By.XPATH, "//*[contains(@class, 'icon-time')]/..").text
-                    length_str = find_first_number(length_text)
-                    length = int(length_str) if length_str.isdigit() else None
+                    length = extract_minutes(length_text)
 
                     rating_str = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/div[2]/a/div/span").text
-                    rating = float(rating_str) if rating_str.replace('.', '', 1).isdigit() else None
+                    rating = float(rating_str.replace(',', '.')) if rating_str.replace(',', '.').replace('.', '', 1).isdigit() else None
 
                     votes_text = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/div[2]/div/span/b[2]").text
                     votes_str = find_first_number(votes_text)
                     votes = int(votes_str) if votes_str.isdigit() else None
 
                     countries = browser.find_element(By.XPATH, "//*[@id='uwee']/div[2]/p[4]").text
+                    countries = '' if countries.isdigit() else countries
+
                     image_link = browser.find_element(By.XPATH, "//*[@id='uwee']/div[1]/div/img").get_attribute("src")
                     link: str = h
 
